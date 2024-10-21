1dcf4a1cbf0c7b833a2e3206e75bc3abb8eeb785
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2024-10-21 22:05:07 +0600

    feat: project initited with tailwind css.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="w-full text-center">
+        <h1 className="text-4xl text-red-400">Hello App!</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
